aeebbe93e6979316db17954319c1d98addcdaf1c
CZutavern <99159571+CZutavern@users.noreply.github.com>
2024-10-30 15:28:57 -0500

    Create Time.java

---
+++ ScheduleBuilder/Time.java
@@ -0,0 +1,16 @@
+public class Time
+{
+  private int time;
+  public Time()
+  {
+    time = 0;
+  }
+  public Time(int time)
+  {
+    this.time = time;
+  }
+  public boolean compareTime(Time time)
+  {
+    
+  }
+}
