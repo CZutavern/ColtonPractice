e9989699ee086cc4646a85644dccc9095f91f1f8
CZutavern <99159571+CZutavern@users.noreply.github.com>
2024-10-30 15:25:39 -0500

    Create Lecture.java

---
+++ ScheduleBuilder/Lecture.java
@@ -0,0 +1,31 @@
+public class Lecture
+{
+  private int[] times;
+  private int length;
+  private int[] times2;
+  private int length2;
+  private boolean hasTwoTimes;
+  private String name;
+  private int credits;
+  
+  public Lecture()
+  {
+    times = [0];
+    length = 0;
+    times2 = [0];
+    length2 = 0;
+    hasTwoTimes = false;
+    name = "";
+    credits = 0;
+  }
+  public Lecture(String times, )
+  {
+    times = [0];
+    length = 0;
+    times2 = [0];
+    length2 = 0;
+    hasTwoTimes = false;
+    name = "";
+    credits = 0;
+  }
+}
