c676ca5db83fd039744f5ecbf8ef0a0edd9c32dc
CZutavern <99159571+CZutavern@users.noreply.github.com>
2024-10-31 14:58:38 -0500

    Update ScheduleBuilder.java

--- ScheduleBuilder/ScheduleBuilder.java
+++ ScheduleBuilder/ScheduleBuilder.java
@@ -1,36 +1,50 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 public class ScheduleBuilder
 {
-  private Lecture[] classes;
+  private ArrayList<Lecture> classes;
+  private ArrayList<ArrayList<int>> takenTimes;
+  private Map<int, Lecture> dict;
+  
   public ScheduleBuilder()
   {
-    
+    dict = new HashMap<int, Lecture>();
+    takenTimes = new ArrayList<ArrayList<Integer>>();
+    classes = new ArrayList<Lecture>();
   }
   public void generateSchedules()
   {
-    
+    for(int i = 0; i < classes.size();i++){
+      ArrayList<ArrayList<
+      
   }
   public void addLectures(File file)
   {
     Lecture temp = new Lecture(file);
     }
   }
+  public printTimes
   public static void main(String[] args)
   {
     ScheduleBuilder schedule = new ScheduleBuilder();
-    
-    try
-    {
-      File file = new File("times.txt");
+    String[] fileNames = ["Phys.txt"];
+    for(int = 0; i < fileNames.length; i++){
+      
+      try
+      {
+        File file = new File("times.txt");
+      }
+      catch (FileNotFoundException e)
+      {
+        System.out.println("An error occured.");
+        e.printStackTrace();
+      }
+      schedule.addLectures(file);
     }
-    catch (FileNotFoundException e)
-    {
-      System.out.println("An error occured.");
-      e.printStackTrace();
-    }
-    schedule.addLectures(file)
+      
     
   }
 }
